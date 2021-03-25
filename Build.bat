64ad019a99eece14ace192790e0c4cce2ff0f900
Econn <Econn50@outlook.com>
2021-03-25 12:42:58 +0000

    Added better gitignore

--- Build.bat
+++
@@ -1,4 +0,0 @@
-dotnet publish -o "Builds/ConwaysGameOfLife Win-x64" -c Release -r win10-x64 --no-self-contained -p:PublishSingleFile=true
-dotnet publish -o "Builds/ConwaysGameOfLife Win-x86" -c Release -r win10-x86 --no-self-contained -p:PublishSingleFile=true
-dotnet publish -o "Builds/ConwaysGameOfLife Linux-x64" -c Release -r linux-x64 --no-self-contained -p:PublishSingleFile=true
-dotnet publish -o "Builds/ConwaysGameOfLife MacOS-x64" -c Release -r osx.10.14-x64 --no-self-contained -p:PublishSingleFile=true
